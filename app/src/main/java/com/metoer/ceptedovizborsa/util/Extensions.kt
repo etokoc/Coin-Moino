cccf1de0a757df2feaf257cf38b2b5416a177912
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-10-13 22:30:21 +0300

    add Extensions.kt

--- app/src/main/java/com/metoer/ceptedovizborsa/util/Extensions.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/util/Extensions.kt
@@ -1,2 +1,31 @@
 package com.metoer.ceptedovizborsa.util
 
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invs() {
+    this.visibility = View.INVISIBLE
+}
+
+fun TextView.textColors(colorId: Int) {
+    this.setTextColor(ContextCompat.getColor(context, colorId))
+}
+
+fun Context.showToastLong(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+}
+
+fun Context.showToastShort(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
