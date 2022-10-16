da9f4e1d9e98d7007c07074841b169f2b571da7a
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-10-16 22:31:05 +0300

    Fix the search.

--- app/src/main/java/com/metoer/ceptedovizborsa/view/fragment/CurrencyFragment.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/view/fragment/CurrencyFragment.kt
@@ -11,14 +11,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.metoer.ceptedovizborsa.R
 import com.metoer.ceptedovizborsa.adapter.CurrencyAdapter
 import com.metoer.ceptedovizborsa.data.response.Currency
-import com.metoer.ceptedovizborsa.data.response.TarihDate
 import com.metoer.ceptedovizborsa.databinding.FragmentCurrencyBinding
-import com.metoer.ceptedovizborsa.util.showToastShort
+import com.metoer.ceptedovizborsa.util.hide
+import com.metoer.ceptedovizborsa.util.show
 import com.metoer.ceptedovizborsa.viewmodel.fragment.CurrencyViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class CurrencyFragment : Fragment() {
@@ -26,12 +24,12 @@ class CurrencyFragment : Fragment() {
     private lateinit var adapter: CurrencyAdapter
     private val viewModel: CurrencyViewModel by hiltNavGraphViewModels(R.id.my_navigation)
     private var _binding: FragmentCurrencyBinding? = null
+    private var currencyList = ArrayList<Currency>()
     private val binding
         get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCurrencyBinding.inflate(inflater, container, false)
         initListeners()
@@ -42,7 +40,7 @@ class CurrencyFragment : Fragment() {
                 }
 
                 override fun onQueryTextChange(searchText: String): Boolean {
-                    filter(searchText)
+                    if (searchText.isNotEmpty()) filter(searchText) else filter(" ")
                     return false
                 }
             })
@@ -52,23 +50,25 @@ class CurrencyFragment : Fragment() {
 
     private fun filter(text: String) {
         val filterlist = ArrayList<Currency>()
-        viewModel.currencyMutableList.observe(viewLifecycleOwner) {
-            for (item in it) {
-                if (item.Isim?.toLowerCase()?.contains(text.lowercase(Locale.getDefault()))!!) {
-                    filterlist.add(item)
-                }
+        for (item in currencyList) {
+            if (item.Isim?.toLowerCase()?.contains(text.lowercase(Locale.getDefault()))!!) {
+                filterlist.add(item)
             }
         }
         if (filterlist.isEmpty()) {
-            context?.showToastShort("Veri Bulunamadı")
+            filterlist.clear()
+            adapter.filterList(filterlist)
+            binding.textviewEmptyMessage.show()
         } else {
             adapter.filterList(filterlist)
+            binding.textviewEmptyMessage.hide()
         }
     }
 
     private fun initListeners() {
         viewModel.currencyMutableList.observe(viewLifecycleOwner) {
-            adapter=CurrencyAdapter(it)
+            adapter = CurrencyAdapter(it)
+            currencyList.addAll(it)
             binding.currencyRecyclerView.adapter = adapter
             binding.currencyRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         }
