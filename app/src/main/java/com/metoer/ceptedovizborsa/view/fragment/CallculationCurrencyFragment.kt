a2e83f489e430a08b6179932b20e43cedb11e830
Ömer Yavuzyiğit <yavuzyigitomer52@gmail.com>
2022-10-18 14:09:26 +0300

    second page calculating succesfull

--- app/src/main/java/com/metoer/ceptedovizborsa/view/fragment/CallculationCurrencyFragment.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/view/fragment/CallculationCurrencyFragment.kt
@@ -1,19 +1,27 @@
 package com.metoer.ceptedovizborsa.view.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.metoer.ceptedovizborsa.data.response.Currency
 import com.metoer.ceptedovizborsa.databinding.FragmentCallculationCurrencyBinding
+import com.metoer.ceptedovizborsa.util.showToastShort
 import com.metoer.ceptedovizborsa.viewmodel.fragment.CallculationCurrencyViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_callculation_currency.*
 import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 @AndroidEntryPoint
@@ -43,6 +51,7 @@ class CallculationCurrencyFragment : Fragment() {
     }
 
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListener() {
         viewmodel.currencyLiveData.observe(viewLifecycleOwner) {
             currencyList.addAll(it)
@@ -51,22 +60,80 @@ class CallculationCurrencyFragment : Fragment() {
 
         var money = 0.0
         binding.apply {
-            monayValueEditText1.addTextChangedListener {
-//                money = if (it!!.isEmpty()) 0.0 else
-//                    it.toString().toDouble()
-//                val result =
-//                    ((currencyList[moneyValueSpinner1.selectedItemPosition].ForexBuying!!) * money) / currencyList[moneyValueSpinner2.selectedItemPosition].ForexBuying!!
-//                monayValueEditText2.setText(result.toString())
-            }
-            monayValueEditText2.addTextChangedListener {
-                money = if (it!!.isEmpty()) 0.0 else
-                    it.toString().toDouble()
-                val money1position = moneyValueSpinner1.selectedItemPosition
-                val money2position = moneyValueSpinner2.selectedItemPosition
-                val result =
-                    ((currencyList[money2position].ForexBuying!! / currencyList[money2position].Unit!!.toDouble()) * money) / currencyList[money1position].ForexBuying!! / currencyList[money1position].Unit!!.toDouble()
-                monayValueEditText1.setText(DecimalFormat("##.####").format(result).toString())
-            }
+            var editControl = false
+            var editControl2 = false
+            monayValueEditText1.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    p0: CharSequence?,
+                    p1: Int,
+                    p2: Int,
+                    p3: Int
+                ) {
+                    editControl = true
+                    Log.i("focus", "Yazmadan Önce")
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0 != null && p0.isNotEmpty()) {
+                        if (editControl && editControl2 == false) {
+                            val format: NumberFormat = NumberFormat.getInstance(Locale.getDefault())
+                            val number: Number = format.parse(p0.toString()) as Number
+                            val d = number.toDouble()
+                            money = d
+                            val money1position = moneyValueSpinner1.selectedItemPosition
+                            val money2position = moneyValueSpinner2.selectedItemPosition
+                            val result =
+                                ((currencyList[money1position].ForexBuying!! / currencyList[money1position].Unit!!.toDouble()) * money) / currencyList[money2position].ForexBuying!! / currencyList[money2position].Unit!!.toDouble()
+                            monayValueEditText2.setText(
+                                DecimalFormat("##.####").format(result).toString()
+                            )
+                            Log.i("focus", "Yazarken")
+                        }
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    editControl = false
+                }
+
+            })
+
+            monayValueEditText2.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    p0: CharSequence?,
+                    p1: Int,
+                    p2: Int,
+                    p3: Int
+                ) {
+                    editControl2 = true
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0 != null && p0.isNotEmpty()) {
+                        val format: NumberFormat = NumberFormat.getInstance(Locale.getDefault())
+                        val number: Number = format.parse(p0.toString()) as Number
+                        val d = number.toDouble()
+
+                        if (editControl2 == true && editControl == false) {
+                            money = d
+                            val money1position = moneyValueSpinner1.selectedItemPosition
+                            val money2position = moneyValueSpinner2.selectedItemPosition
+                            val result =
+                                ((currencyList[money2position].ForexBuying!! / currencyList[money2position].Unit!!.toDouble()) * money) / currencyList[money1position].ForexBuying!! / currencyList[money1position].Unit!!.toDouble()
+                            monayValueEditText1.setText(
+                                DecimalFormat("##.####").format(result).toString()
+                            )
+                        }
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    editControl2 = false
+
+                }
+
+            })
+
         }
 
     }
@@ -95,20 +162,20 @@ class CallculationCurrencyFragment : Fragment() {
                         return
                     }
                 }
-            moneyValueSpinner2.onItemSelectedListener =
-                object : AdapterView.OnItemSelectedListener {
-                    override fun onItemSelected(
-                        adapterView: AdapterView<*>?,
-                        view: View,
-                        i: Int,
-                        l: Long
-                    ) {
-                    }
-
-                    override fun onNothingSelected(adapterView: AdapterView<*>?) {
-                        return
-                    }
+            moneyValueSpinner2.onItemSelectedListener
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    adapterView: AdapterView<*>?,
+                    view: View,
+                    i: Int,
+                    l: Long
+                ) {
                 }
+
+                override fun onNothingSelected(adapterView: AdapterView<*>?) {
+                    return
+                }
+            }
         }
     }
 
