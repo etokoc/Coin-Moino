00d2fd463852a818ccfc5e44652670c457e07be6
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-10-25 00:34:04 +0300

    add binding for recylerviewadapter

--- app/src/main/java/com/metoer/ceptedovizborsa/adapter/CurrencyAdapter.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/adapter/CurrencyAdapter.kt
@@ -1,13 +1,13 @@
 package com.metoer.ceptedovizborsa.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil.calculateDiff
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.metoer.ceptedovizborsa.R
 import com.metoer.ceptedovizborsa.data.response.currency.Currency
+import com.metoer.ceptedovizborsa.databinding.CurrencyItemListBinding
 import com.metoer.ceptedovizborsa.util.Constants
 import kotlinx.android.synthetic.main.currency_item_list.view.*
 import java.text.DecimalFormat
@@ -15,7 +15,9 @@ import java.util.*
 
 class CurrencyAdapter :
     RecyclerView.Adapter<CurrencyAdapter.ListViewHolder>() {
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    class ListViewHolder(val binding: CurrencyItemListBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     fun filterList(filterList: List<Currency>) {
         setData(filterList)
@@ -23,19 +25,18 @@ class CurrencyAdapter :
 
     private var oldItemList = emptyList<Currency>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.currency_item_list, parent, false)
+        val view = CurrencyItemListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val currentItems = oldItemList[position]
-        holder.itemView.apply {
+        holder.binding.apply {
             moneyNameTextView.text = currentItems.Isim
             moneyCodeTextView.text = currentItems.CurrencyCode
             val result = currentItems.ForexBuying?.div(currentItems.Unit!!)!!
             moneyValueTextView.text = "₺" + DecimalFormat("##.####").format(result)
-            Glide.with(this).load(
+            Glide.with(this.root).load(
                 Constants.IMAGE_URL + "${
                     currentItems.CurrencyCode?.lowercase(
                         Locale.ENGLISH
