305fe4127e615afee5a989ec1c2707c91856a176
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-10-27 16:30:26 +0300

    menu rank has changed

--- app/src/main/java/com/metoer/ceptedovizborsa/view/activity/MainActivity.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/view/activity/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.metoer.ceptedovizborsa.view.activity
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.metoer.ceptedovizborsa.R
-import com.metoer.ceptedovizborsa.data.ApiNetworkAdapter
 import com.metoer.ceptedovizborsa.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.apply {
             val navController = findNavController(R.id.home_fragment)
-            clockMenuBar.setupWithNavController(navController)
+            bottomMenuBar.setupWithNavController(navController)
         }
     }
 
