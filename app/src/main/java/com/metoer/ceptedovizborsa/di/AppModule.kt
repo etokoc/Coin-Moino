90723d94360c164ffc49364534d122f6a6133b64
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-11-21 17:21:45 +0300

    dagger - hilt mimarisine uygun retrofit kullanımı ve servis kulalnımı düzeltildi

--- app/src/main/java/com/metoer/ceptedovizborsa/di/AppModule.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/di/AppModule.kt
@@ -1,14 +1,22 @@
 package com.metoer.ceptedovizborsa.di
 
-import com.metoer.ceptedovizborsa.data.ApiNetworkAdapter
+import android.util.Log
 import com.metoer.ceptedovizborsa.data.AppApi
+import com.metoer.ceptedovizborsa.data.QualifiedTypeConverterFactory
 import com.metoer.ceptedovizborsa.data.repository.CurrencyRepository
-import com.metoer.ceptedovizborsa.view.fragment.CoinFragment
+import com.metoer.ceptedovizborsa.util.Constants
 import com.metoer.ceptedovizborsa.viewmodel.fragment.SharedViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -22,14 +30,48 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesAppApi(): AppApi {
-        return ApiNetworkAdapter.appApi()
-    }
-    @Provides
-    @Singleton
     fun providesSharedViewModel(): SharedViewModel {
         return SharedViewModel()
     }
+
+    @Provides
+    @Singleton
+    fun providesOkhttp(): OkHttpClient.Builder {
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .addNetworkInterceptor(HttpLoggingInterceptor { message ->
+                Log.d(
+                    "tag_retrofit_intercept",
+                    "log: http log: $message"
+                )
+            }.setLevel(HttpLoggingInterceptor.Level.BODY))
+        return builder
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit (providesOkHttpClient: OkHttpClient.Builder) : Retrofit{
+        val retrofit = Retrofit
+            .Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(
+                QualifiedTypeConverterFactory(
+                    GsonConverterFactory.create(),
+                    SimpleXmlConverterFactory.create()
+                )
+            )
+            .baseUrl(Constants.CURRENCY_BASE_URL)
+            .client(providesOkHttpClient.build())
+            .build()
+
+        return retrofit
+    }
+
+    @Provides
+    @Singleton
+    fun providesAppApi(retrofit: Retrofit) =retrofit.create(AppApi::class.java)
 
 
 }
