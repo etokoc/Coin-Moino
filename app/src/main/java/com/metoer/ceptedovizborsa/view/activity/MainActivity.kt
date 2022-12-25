bbf5d115e4db7eb32f6ce152fa740c546ba16df5
Ömer Yavuzyiğit <yavuzyigitomer52@gmail.com>
2022-12-25 21:40:28 +0300

    drawer menü adding

--- app/src/main/java/com/metoer/ceptedovizborsa/view/activity/MainActivity.kt
+++ app/src/main/java/com/metoer/ceptedovizborsa/view/activity/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.metoer.ceptedovizborsa.view.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.metoer.ceptedovizborsa.R
 import com.metoer.ceptedovizborsa.databinding.ActivityMainBinding
+import com.metoer.ceptedovizborsa.util.showToastShort
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +27,42 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             val navController = findNavController(R.id.home_fragment)
             bottomMenuBar.setupWithNavController(navController)
+            setSupportActionBar(appBar)
+
+            toggle = ActionBarDrawerToggle(
+                this@MainActivity,
+                mainDrawerLayout,
+                R.string.open,
+                R.string.close
+            )
+            mainDrawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            mainNav.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.setting_menu -> {
+
+                    }
+                    R.id.about_menu -> {
+
+                    }
+                }
+                true
+            }
         }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (binding.mainDrawerLayout.isDrawerOpen(GravityCompat.END)) {
+            binding.mainDrawerLayout.closeDrawer(GravityCompat.END)
+            if (toggle.onOptionsItemSelected(item)) {
+                return true
+            }
+        } else {
+            binding.mainDrawerLayout.openDrawer(GravityCompat.END)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onResume() {
